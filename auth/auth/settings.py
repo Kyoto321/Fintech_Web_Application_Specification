5e051689b1a0bdf7c3d95d029dfbc541e875444f
Ayo Bankole <bankoleayodele147@gmail.com>
2025-08-05 12:33:53 +0100

    business transaction payments

--- auth/auth/settings.py
+++ auth/auth/settings.py
@@ -166,12 +166,7 @@ PAGE_SIZE = 5
 
 
 # EMAIL CONFIG
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-EMAIL_HOST = config("EMAIL_HOST")
-EMAIL_HOST_USER = config("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+
 
 
